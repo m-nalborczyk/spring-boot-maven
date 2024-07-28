5db85c415ccb3b55676397dc0b5330849795d64e
nalboro <stev1989@gmail.com>
2024-07-28 09:03:40 +0200

    commit UserService interface

---
+++ src/main/java/spring/course/services/UserService.java
@@ -0,0 +1,11 @@
+package spring.course.services;
+
+import spring.course.model.UserModel;
+
+public interface UserService {
+
+    UserModel getUser(String userName);
+    void addUser(UserModel userName);
+    void deleteUser(String userName);
+
+}

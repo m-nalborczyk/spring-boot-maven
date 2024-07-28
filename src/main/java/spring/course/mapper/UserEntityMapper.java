c2f010c51c5501dbaf2d4c052b4a707c34472fed
nalboro <stev1989@gmail.com>
2024-07-28 12:38:56 +0200

    commit UserEntityMapper

---
+++ src/main/java/spring/course/mapper/UserEntityMapper.java
@@ -0,0 +1,30 @@
+package spring.course.mapper;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
+import spring.course.data.UserEntity;
+import spring.course.model.UserModel;
+
+@Component
+public class UserEntityMapper implements EntityMapper<UserEntity, UserModel> {
+    @Override
+    public UserModel map(UserEntity entity) {
+
+        return new UserModel(
+                entity.getFirstName(),
+                entity.getLastName(),
+                entity.getMembershipId(),
+                entity.getCreationTime()
+        );
+    }
+
+    @Override
+    public UserEntity reverseMap(UserModel entity) {
+        return new UserEntity(
+                entity.getFirstName(),
+                entity.getLastName(),
+                entity.getMembershipId(),
+                entity.getCreationTime()
+        );
+    }
+}

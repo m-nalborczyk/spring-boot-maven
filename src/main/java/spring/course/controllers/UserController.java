a6c82d9f29049ae78cfc5bd18c23b7e0a7882300
nalboro <stev1989@gmail.com>
2024-07-28 09:03:01 +0200

    commit UserController change

--- src/main/java/spring/course/controllers/UserController.java
+++ src/main/java/spring/course/controllers/UserController.java
@@ -1,26 +1,39 @@
 package spring.course.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import spring.course.model.UserModel;
+import spring.course.services.UserService;
+import spring.course.services.UserServiceImplementation;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
-    private Map<String, UserModel> userMap = new HashMap<>();
+    private UserService service;
 
-    public UserController() {
-        userMap.put("John", new UserModel("John", "Doe", 1234));
-        userMap.put("Jane", new UserModel("Jane", "Doe", 5432));
+
+    public UserController(@Qualifier("primary") UserService userService) {
+        this.service = userService;
     }
 
     @GetMapping("/getUser/{userName}")
-    public UserModel getUser(@PathVariable("userName")String userName){
-        return userMap.get(userName);
+    public ResponseEntity<UserModel> getUser(@PathVariable("userName")String userName){
+        return ResponseEntity.of(Optional.ofNullable(service.getUser(userName)));
+    }
+
+    @PostMapping("/addUser")
+    public ResponseEntity<HttpStatus> createUser(@RequestBody UserModel user){
+        service.addUser(user);
+        return ResponseEntity.accepted().build();
+    }
+    @DeleteMapping("/deleteUser/{userName}")
+    public ResponseEntity<HttpStatus> deleteUser (@PathVariable("userName") String userName){
+        service.deleteUser(userName);
+        return ResponseEntity.noContent().build();
     }
 
 }

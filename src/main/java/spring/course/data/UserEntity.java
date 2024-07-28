9fb8e2cf9e9ad0705c31744a48a6afbbc49b4123
nalboro <stev1989@gmail.com>
2024-07-28 12:38:45 +0200

    commit UserEntity

---
+++ src/main/java/spring/course/data/UserEntity.java
@@ -0,0 +1,58 @@
+package spring.course.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+@Entity
+public class UserEntity implements Serializable {
+
+    @Id
+    private String firstName;
+    private String lastName;
+    private Integer membershipId;
+
+    private String creationTime;
+
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String firstName, String lastName, Integer membershipId, String creationTime) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.membershipId = membershipId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(Integer membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+}
